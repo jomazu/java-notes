e88948d077c1c599b7841eda19d361aef533aba2
John Zukowski <johnz@jomazu.io>
2016-09-01 22:45:45 -0400

    Second fix to formatting.

--- Milk.java
+++ Milk.java
@@ -7,18 +7,17 @@ import java.io.Console;
 public class DoWhile {
 
   public static void main(String[] args) {
-  Console console = System.console();
+    Console console = System.console();
+    String response;
+    boolean isNo;
 
-  String response;
-  boolean isNo;
-
-    do {
-      response = console.readLine("Do you understand do while loops?  ");
-      isNo = (response.equalsIgnoreCase("No"));
-      if (isNo) {
-        console.printf("Because you said %s, you passed the test!\n", response);
-        System.exit(0);
-      }
-    } while(isNo);
+      do {
+        response = console.readLine("Do you understand do while loops?  ");
+        isNo = (response.equalsIgnoreCase("No"));
+        if (isNo) {
+          console.printf("Because you said %s, you did not pass the test!\n", response);
+          System.exit(0);
+        }
+      } while(isNo);
   }
 }
