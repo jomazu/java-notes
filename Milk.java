a19a3f88de719b5f73f360fd2b7da71fa986bfc5
John Zukowski <johnz@jomazu.io>
2016-09-01 22:39:37 -0400

    Corrected Milk.java to reflect Google Java Style Guide.

--- Milk.java
+++ Milk.java
@@ -1,11 +1,24 @@
+/**
+* Javadoc example ...
+*/
+
 import java.io.Console;
 
-public class Milk {
+public class DoWhile {
 
-    public static void main(String[] args) {
-        Console console = System.console();
-        for (int i = 99; i > 0; i--) {
-            console.printf("%d bottles of milk on the wall... %n", i);
-        }
-    }
+  public static void main(String[] args) {
+  Console console = System.console();
+
+  String response;
+  boolean isNo;
+
+    do {
+      response = console.readLine("Do you understand do while loops?  ");
+      isNo = (response.equalsIgnoreCase("No"));
+      if (isNo) {
+        console.printf("Because you said %s, you passed the test!\n", response);
+        System.exit(0);
+      }
+    } while(isNo);
+  }
 }
